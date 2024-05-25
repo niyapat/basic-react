24e0dbb995b8433028ded39fa9f4306f0c66b122
Niyati <150647433+niyapat@users.noreply.github.com>
2024-05-25 00:45:57 -0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./componets/Navbar";
+import Main from "./componets/Main";
+
+export const App = () => {
+	return (
+		<>
+			<Navbar></Navbar>
+			<main></main>
+			<h1>Hello World!</h1>
+			<p>THis is the paragraph</p>
+		</>
+	);
+};
+
+export default App;
---
+++ src/componets/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Navbar = () => {
+	return (
+		<>
+			<a href="#">Home</a>
+			<a href="#">About</a>
+			<a href="#">Contact Us</a>
+			<a href="#">Register</a>
+		</>
+	);
+};
+
+export default Navbar;
